4510c580b5a2da33c16128a9e74a680b3d569611
Harinder Singh <kinggouken611@gmail.com>
2022-02-16 00:42:11 +0530

    Auto Logout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
 import Page from '../components/Page';
-import PleaseSignIn from '../components/PleaseSignIn';
 
 export default function IndexPage() {
   return (
     <div>
-      {/* <PleaseSignIn> */}
       <Page />
-      {/* </PleaseSignIn> */}
     </div>
   );
 }
